96ee04d6dd38f6b9972ebab78585d1f84511a135
MhZ9191 <hegic91@gmail.com>
2026-01-12 16:52:13 +0100

    Added button color

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 const imageLamp = document.querySelector(".div-image>img");
-console.log(imageLamp.src);
 
 const btn = document.getElementById("btnUni");
 
-// btn.addEventListener("click", function () {
-//   imageLamp.src = "./img/yellow_lamp.png";
-// });
-
 btn.addEventListener("click", function () {
   btnText = btn.innerText;
+  const btnClass = btn.classList;
   if (btnText.toLocaleLowerCase().includes("accendi")) {
     imageLamp.src = "./img/yellow_lamp.png";
     btn.innerText = "SPEGNI";
+    btnClass.remove("accesa");
+    btnClass.add("spenta");
   }
   if (btnText.toLocaleLowerCase().includes("spegni")) {
     imageLamp.src = "./img/white_lamp.png";
     btn.innerText = "ACCENDI";
+    btnClass.remove("spenta");
+    btnClass.add("accesa");
   }
 });
