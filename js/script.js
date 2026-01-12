e137471bbe98062fe025dfc1bffdd17e3cb550e8
MhZ9191 <hegic91@gmail.com>
2026-01-12 16:42:44 +0100

    Added lamp and button change

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@ console.log(imageLamp.src);
 
 const btn = document.getElementById("btnUni");
 
+// btn.addEventListener("click", function () {
+//   imageLamp.src = "./img/yellow_lamp.png";
+// });
+
 btn.addEventListener("click", function () {
-  imageLamp.src = "./img/yellow_lamp.png";
+  btnText = btn.innerText;
+  if (btnText.toLocaleLowerCase().includes("accendi")) {
+    imageLamp.src = "./img/yellow_lamp.png";
+    btn.innerText = "SPEGNI";
+  }
+  if (btnText.toLocaleLowerCase().includes("spegni")) {
+    imageLamp.src = "./img/white_lamp.png";
+    btn.innerText = "ACCENDI";
+  }
 });
