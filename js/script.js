97b9f8b2fafac37b0ce8066304d0b882249b88a7
MhZ9191 <hegic91@gmail.com>
2026-01-12 17:15:22 +0100

    Added mousemove coordinate

--- js/script.js
+++ js/script.js
@@ -18,3 +18,20 @@ btn.addEventListener("click", function () {
     btnClass.add("accesa");
   }
 });
+
+const scrX = document.getElementById("scX");
+const scrY = document.getElementById("scY");
+const pgX = document.getElementById("pX");
+const pgY = document.getElementById("pY");
+const clX = document.getElementById("cX");
+const clY = document.getElementById("cY");
+const testBody = document.querySelector("body");
+
+testBody.addEventListener("mousemove", function (e) {
+  scrX.innerText = e.screenX;
+  scrY.innerText = e.screenY;
+  pgX.innerText = e.pageX;
+  pgY.innerText = e.pageY;
+  clX.innerText = e.clientX;
+  clY.innerText = e.clientY;
+});
